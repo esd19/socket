a5347d99c5ca4030815c4f4068cae164d8242df1
esd19 <40478879+esd19@users.noreply.github.com>
2024-11-26 10:52:18 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const WebSocket = require('ws')
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT })
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Mensaje recibido => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+})
