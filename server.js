040e4f2e0efa33158738a0fe76aa4d5adff51c5f
esd19 <40478879+esd19@users.noreply.github.com>
2024-12-12 12:34:05 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 const PORT = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({ port: PORT });
@@ -6,36 +6,119 @@ const wss = new WebSocket.Server({ port: PORT });
 // Lista de clientes conectados
 let clients = [];
 
-wss.on('connection', ws => {
+wss.on("connection", (ws) => {
   // Agregar el cliente a la lista
   clients.push(ws);
-  console.log('Nuevo cliente conectado');
+  console.log("Nuevo cliente conectado");
 
   // Escuchar mensajes del cliente
-  ws.on('message', (message) => {
+  ws.on("message", (message) => {
     const messageText = message.toString(); // Convertir el mensaje a string
-    console.log('Mensaje recibido del cliente:', messageText);
+    console.log("Mensaje recibido del cliente:", messageText);
 
     // Responder con base en el mensaje recibido
     let response;
     switch (messageText) {
-      case '1':
-        response = 'Pregunta 1';
+      case "actualizar":
+        response = "actualizar";
         break;
-      case '2':
-        response = 'Pregunta 2';
+      case "tabla":
+        response = "tabla";
         break;
-      case '3':
-        response = 'Pregunta 3';
+
+      case "1":
+        response = "Pregunta 1";
         break;
-      case '4':
-        response = 'Pregunta 4';
+      case "2":
+        response = "Pregunta 2";
         break;
-      case '5':
-        response = 'Pregunta 5';
+      case "3":
+        response = "Pregunta 3";
         break;
+      case "4":
+        response = "Pregunta 4";
+        break;
+      case "5":
+        response = "Pregunta 5";
+        break;
+      case "6":
+        response = "Pregunta 6";
+        break;
+      case "7":
+        response = "Pregunta 7";
+        break;
+      case "8":
+        response = "Pregunta 8";
+        break;
+      case "9":
+        response = "Pregunta 9";
+        break;
+      case "10":
+        response = "Pregunta 10";
+        break;
+      case "11":
+        response = "Pregunta 11";
+        break;
+      case "12":
+        response = "Pregunta 12";
+        break;
+      case "13":
+        response = "Pregunta 13";
+        break;
+      case "14":
+        response = "Pregunta 14";
+        break;
+      case "15":
+        response = "Pregunta 15";
+        break;
+      case "16":
+        response = "Pregunta 16";
+        break;
+      case "17":
+        response = "Pregunta 17";
+        break;
+      case "18":
+        response = "Pregunta 18";
+        break;
+      case "19":
+        response = "Pregunta 19";
+        break;
+      case "20":
+        response = "Pregunta 20";
+        break;
+      case "21":
+        response = "Pregunta 21";
+        break;
+      case "22":
+        response = "Pregunta 22";
+        break;
+      case "23":
+        response = "Pregunta 23";
+        break;
+      case "24":
+        response = "Pregunta 24";
+        break;
+      case "25":
+        response = "Pregunta 25";
+        break;
+      case "26":
+        response = "Pregunta 26";
+        break;
+      case "27":
+        response = "Pregunta 27";
+        break;
+      case "28":
+        response = "Pregunta 28";
+        break;
+      case "29":
+        response = "Pregunta 29";
+        break;
+      case "30":
+        response = "Pregunta 30";
+        break;
+
       default:
-        response = 'Mensaje no válido';
+        response = "Pregunta no valida";
         break;
     }
 
@@ -48,11 +131,11 @@ wss.on('connection', ws => {
   });
 
   // Manejar la desconexión del cliente
-  ws.on('close', () => {
-    console.log('Cliente desconectado');
-    clients = clients.filter(client => client !== ws); // Remover cliente de la lista
+  ws.on("close", () => {
+    console.log("Cliente desconectado");
+    clients = clients.filter((client) => client !== ws); // Remover cliente de la lista
   });
 
   // Enviar mensaje inicial al cliente
-  ws.send('Hello! Message From Server!!');
+  ws.send("Hello! Message From Server!!");
 });
